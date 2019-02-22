aaac69282728cd7465d8fee604f2c43d1bb0d355
Harald Sitter <sitter@kde.org>
2019-02-22 13:48:20 +0100

    implement on-demand dependency resolution & replace mkmf with custom code
    this is a bit meh but since neondocker needs docker but we don't want
    docker on the ISO we have to jump through hoops to get this working
    out of the box :(
    
    xephyr and docker are now installed unless they are available. docker-api
    gem is also installed as necessary.
    
    the latter is a bit tricky even. when not run as a program the file will
    expect the gem to be present (as before) but when run as program it will
    install the gem and then reexecute the program to reload the gem list.
    to prevent inifnity reexecs there also needs to be a loop protection
    passed via the environment
    
    this also replaces the use of mkmf with a custom Executable class to locate
    binaries. mkmf is not meant to be used outside build configuration. it
    pollutes the main object namespace and it actually requires ruby-dev to
    run. it's altogether rubbish here. instead the new Executable class from
    releaseme implements proper binary lookup but without all the downsides
    of mkmf

--- neondocker/neondocker.rb
+++ neondocker/neondocker.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 # Copyright 2017 Jonathan Riddell <jr@jriddell.org>
+# Copyright 2015-2019 Harald Sitter <sitter@kde.org>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -18,14 +19,98 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-begin
-  require 'docker'
-rescue LoadError
-  puts 'Could not find docker-api library, run: sudo gem install docker-api'
-  exit 1
+if $PROGRAM_NAME != __FILE__
+  # Note that during program execution docker is required in the exec block
+  # as it gets on-demand installed if applicable.
+  begin
+    require 'docker'
+  rescue LoadError
+    puts 'Could not find docker-api library, run: sudo gem install docker-api'
+    exit 1
+  end
 end
+
+require 'etc'
 require 'optparse'
-require 'mkmf'
+require 'shellwords'
+
+# Finds executables. MakeMakefile is the only core ruby entity providing
+# PATH based executable lookup, unfortunately it is really not meant to be
+# used outside extconf.rb use cases as it mangles the main name scope by
+# injecting itself into it (which breaks for example the ffi gem).
+# The Shell interface's command-processor also has lookup code but it's not
+# Windows compatible.
+# NB: this is lifted from releaseme! should this need changing, change it there
+# first! also mind the unit test.
+class Executable
+  attr_reader :bin
+
+  def initialize(bin)
+    @bin = bin
+  end
+
+  # Finds the executable in PATH by joining it with all parts of PATH and
+  # checking if the resulting absolute path exists and is an executable.
+  # This also honor's Windows' PATHEXT to determine the list of potential
+  # file extensions. So find('gpg2') will find gpg2 on POSIX and gpg2.exe
+  # on Windows.
+  def find
+    # PATHEXT on Windows defines the valid executable extensions.
+    exts = ENV.fetch('PATHEXT', '').split(';')
+    # On other systems we'll work with no extensions.
+    exts << '' if exts.empty?
+
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      path = unescape_path(path)
+      exts.each do |ext|
+        file = File.join(path, bin + ext)
+        return file if executable?(file)
+      end
+    end
+
+    nil
+  end
+
+  private
+
+  class << self
+    def windows?
+      @windows ||= ENV['RELEASEME_FORCE_WINDOWS'] || mswin? || mingw?
+    end
+
+    private
+
+    def mswin?
+      @mswin ||= /mswin/ =~ RUBY_PLATFORM
+    end
+
+    def mingw?
+      @mingw ||= /mingw/ =~ RUBY_PLATFORM
+    end
+  end
+
+  def windows?
+    self.class.windows?
+  end
+
+  def executable?(path)
+    stat = File.stat(path)
+  rescue SystemCallError
+  else
+    return true if stat.file? && stat.executable?
+  end
+
+  def unescape_path(path)
+    # Strip qutation.
+    # NB: POSIX does not define any quoting mechanism so you simply cannot
+    # have colons in PATH on POSIX systems as a side effect we mustn't
+    # strip quotes as they have no syntactic meaning and instead are
+    # assumed to be part of the path
+    # http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html#tag_08_03
+    return path.sub(/\A"(.*)"\z/m, '\1') if windows?
+    path
+  end
+end
 
 # A wee command to simplify running KDE neon Docker images.
 #
@@ -128,7 +213,7 @@ class NeonDocker
 
   # Is the command available to run?
   def installed?(command)
-    MakeMakefile.find_executable(command)
+    Executable.new(command).find
   end
 
   def running_xhost
@@ -212,7 +297,227 @@ class NeonDocker
   end
 end
 
+# Jiggles dependencies into place.
+
+# Install deb dependencies.
+class DebDependencies
+  def run
+    pkgs_to_install = []
+    pkgs_to_install << 'docker.io' unless File.exist?('/var/run/docker.sock')
+    pkgs_to_install << 'xserver-xephyr' unless Executable.new('Xephyr').find
+
+    return if pkgs_to_install.empty?
+
+    warn 'Some packages need installing to use neondocker...'
+    system('pkcon', 'install', *pkgs_to_install) || raise
+  end
+end
+
+# Install !core gem dependencies and re-execs.
+class GemDependencies
+  def run
+    require 'docker'
+  rescue LoadError
+    if ENV['NEONDOCKER_REEXC']
+      abort 'E: Installing ruby dependencies failed -> bugs.kde.org'
+    end
+    warn 'Some ruby dependencies need installing to use neondocker...'
+    system('pkexec', 'gem', 'install', '--no-document', 'docker-api')
+    ENV['NEONDOCKER_REEXC'] = '1'
+    puts '...reexecuting...'
+    exec(__FILE__, *ARGV)
+  end
+end
+
+# Switchs group through re-exec.
+class GroupDependencies
+  DOCKER_GROUP = 'docker'.freeze
+
+  def run
+    return if Process.uid.zero? # root always has access
+    return if Process.groups.include?(docker_gid)
+
+    unless user_in_group?
+      adduser? || raise # adduser? actually aborts, the raise is just sugar
+      system('pkexec', 'adduser', Etc.getlogin, DOCKER_GROUP) || raise
+    end
+
+    puts '...reexecuting with docker access...'
+    exec('sg', DOCKER_GROUP, '-c', __FILE__, *ARGV)
+  end
+
+  private
+
+  def user_in_group?
+    member = false
+    Etc.group do |group|
+      member = group.mem.include?(Etc.getlogin) if group.name == DOCKER_GROUP
+    end
+    member
+  end
+
+  def adduser?
+    loop do
+      puts <<~QUESTION
+        You currently do not have access to the docker socket. Do you want to
+        give this user access? [Y/n]
+      QUESTION
+
+      input = gets.strip
+      if input.casecmp('n').zero?
+        abort <<~MSG
+          Without socket access you need to use pkexec or sudo to run neondocker
+        MSG
+      end
+
+      return true if input.casecmp('y').zero?
+    end
+    false
+  end
+
+  def docker_gid
+    @docker_gid ||= begin
+      gid = nil
+      Etc.group do |group|
+        gid = group.gid if group.name == DOCKER_GROUP
+      end
+      gid
+    end
+  end
+end
+
+# Jiggles dependencies into place.
+class DependencyJiggler
+  def run
+    DebDependencies.new.run
+    GemDependencies.new.run
+    GroupDependencies.new.run
+  end
+end
+
+# Parses Linux os-release files.
+#
+# Variables from os-release are accessible through constants.
+#
+# @example Put os-release 'ID' of current system
+#   puts OSRelease::ID
+#
+# @note When running on potential !Linux or legacy systems you'll need to check
+#   {#available?} before accessing constants, otherwise you may encounter
+#   {NotFoundError} exceptions.
+#
+# @see https://www.freedesktop.org/software/systemd/man/os-release.html
+module OSRelease
+  # Raised when no default os-release file could be found.
+  class NotFoundError < StandardError; end
+
+  class << self
+    # @return [Boolean] true when an os-release file was found in default
+    #   locations as per the os-release specification
+    def available?
+      default_path
+      true
+    rescue NotFoundError
+      false
+    end
+
+    # @param key [Symbol] variable name in the os-release file
+    # @return [Boolean] true when the variable key is defined in the os-release
+    #   data
+    def variable?(key)
+      data.key?(key)
+    end
+
+    # Behaves exactly like {Hash#fetch}.
+    #
+    # @return value of variable (if it is defined see {#variable?})
+    def value(key, default = nil, &block)
+      data.fetch(key, default, &block)
+    end
+
+    # @api private
+    def load!(path = default_path)
+      @data = default_data.dup
+      File.read(path).split("\n").each do |line|
+        # Split by comment to also drop leading and trailing comments. Then
+        # strip to possibly reduce to an empty line.
+        # Note that trailing comments are technically not defined by the spec.
+        line = line.split('#', 2)[0].strip
+        next if line.empty?
+
+        key, value = parse(line)
+        @data[key.to_sym] = value
+      end
+      @data
+    end
+
+    # @api private
+    def reset!
+      @data = nil
+    end
+
+    # @api private
+    def const_missing(name)
+      return value(name) if variable?(name)
+
+      super
+    end
+
+    private
+
+    STRINGLISTS = %w[ID_LIKE].freeze
+
+    def parse(line)
+      key, value = line.split('=', 2)
+      return parse_list(key, value) if STRINGLISTS.include?(key)
+
+      parse_string(key, value)
+    end
+
+    def parse_list(key, value)
+      # If the value is quoted split twice. This is effectively the same
+      # as dropping the quotes. ID_LIKE derives from ID and is therefore
+      # super restricted in what it may contain so that a double split has
+      # no adverse affects.
+      value = Shellwords.split(value)[0] if value.start_with?('"')
+      [key, Shellwords.split(value)]
+    end
+
+    def parse_string(key, value)
+      value = Shellwords.split(value)
+      [key, value[0]]
+    end
+
+    def data
+      @data ||= load!
+    end
+
+    def default_data
+      # Spec defines some variables with a default value.
+      {
+        ID: 'linux',
+        NAME: 'Linux',
+        PRETTY_NAME: 'Linux'
+      }
+    end
+
+    def default_path
+      paths = %w[/etc/os-release /usr/lib/os-release]
+      path = paths.find { |x| File.exist?(x) }
+      return path if path
+
+      raise NotFoundError,
+            "Could not find os-release file in default locations: #{paths}"
+    end
+  end
+end
+
 if $PROGRAM_NAME == __FILE__
+  if OSRelease.available? && (OSRelease::ID == 'ubuntu' ||
+                              OSRelease::ID_LIKE.include?('ubuntu'))
+    DependencyJiggler.new.run
+  end
+
   neon_docker = NeonDocker.new
   options = neon_docker.command_options
   neon_docker.validate_docker
